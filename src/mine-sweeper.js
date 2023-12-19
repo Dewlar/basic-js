9beda69db470d5a6b4dda36a025948b752d5f291
Andrei Markouski <insawm@gmail.com>
2023-12-19 17:42:11 +0300

    feat: matrix-elements-sum && mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,9 +23,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let mineSweeperMatrix = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    mineSweeperMatrix.push([]);
+    for (let j = 0; j < matrix[i].length; j++) {
+      let count = 0;
+      let rowStart = Math.max(i - 1, 0);
+      let rowEnd = Math.min(i + 1, matrix.length - 1);
+      let colStart = Math.max(j - 1, 0);
+      let colEnd = Math.min(j + 1, matrix[0].length - 1);
+
+      for (let x = rowStart; x <= rowEnd; x++) {
+        for (let y = colStart; y <= colEnd; y++) {
+          if (x !== i || y !== j) matrix[x][y] && count++;
+        }
+      }
+      mineSweeperMatrix[i].push(count);
+    }
+  }
+  return mineSweeperMatrix;
 }
 
 module.exports = {
