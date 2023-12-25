53a57688756731cfbb7d4eca923e033b975244a1
Andrei Markouski <insawm@gmail.com>
2023-12-25 21:44:56 +0300

    refactor: moved the calculations into a separate function in mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -29,23 +29,28 @@ function minesweeper(matrix) {
   for (let i = 0; i < matrix.length; i++) {
     mineSweeperMatrix.push([]);
     for (let j = 0; j < matrix[i].length; j++) {
-      let count = 0;
-      let rowStart = Math.max(i - 1, 0);
-      let rowEnd = Math.min(i + 1, matrix.length - 1);
-      let colStart = Math.max(j - 1, 0);
-      let colEnd = Math.min(j + 1, matrix[0].length - 1);
-
-      for (let x = rowStart; x <= rowEnd; x++) {
-        for (let y = colStart; y <= colEnd; y++) {
-          if (x !== i || y !== j) matrix[x][y] && count++;
-        }
-      }
-      mineSweeperMatrix[i].push(count);
+      let mineCount = mineCounter(i, j, matrix);
+      mineSweeperMatrix[i].push(mineCount);
     }
   }
   return mineSweeperMatrix;
 }
 
+function mineCounter(row, col, matrix) {
+  let count = 0;
+  let rowStart = Math.max(row - 1, 0);
+  let rowEnd = Math.min(row + 1, matrix.length - 1);
+  let colStart = Math.max(col - 1, 0);
+  let colEnd = Math.min(col + 1, matrix[0].length - 1);
+
+  for (let x = rowStart; x <= rowEnd; x++) {
+    for (let y = colStart; y <= colEnd; y++) {
+      if (x !== row || y !== col) matrix[x][y] && count++;
+    }
+  }
+  return count;
+}
+
 module.exports = {
-  minesweeper
+  minesweeper,
 };
