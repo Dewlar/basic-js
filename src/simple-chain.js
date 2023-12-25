adf9938577aa527582a3720a325c47fcaf4b94fd
Andrei Markouski <insawm@gmail.com>
2023-12-25 22:03:32 +0300

    refactor: delete console.log & simplify chain check conditions

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,26 +6,30 @@
  */
 const chainMaker = {
   chain: [],
+
   getLength() {
     return this.chain.length;
   },
+
   addLink(value) {
     this.chain.push(`( ${value} )`);
-    console.log(this.chain);
     return this;
   },
+
   removeLink(position) {
-    if (!Number.isInteger(position) || position < 1 || position > this.chain.length) {
+    if (!this.chain[position - 1]) {
       this.chain = [];
       throw new Error("You can't remove incorrect link!");
     }
     this.chain.splice(position - 1, 1);
     return this;
   },
+
   reverseChain() {
     this.chain.reverse();
     return this;
   },
+
   finishChain() {
     let resultString = this.chain.join('~~');
     this.chain = [];
